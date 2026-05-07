2e40622126c7094c4682e3a75304ffa6ac57bdbc
Matoky18 <matokyranay@gmail.com>
2026-05-07 06:00:52 +0300

    marquee desktop

---
+++ script.js
@@ -0,0 +1,14 @@
+// const container = document.querySelector('.marquee');
+// const list = container.querySelector('ul');
+// const items = [...list.querySelectorAll('li')];
+// items.forEach(item=>list.append(item.cloneNode(true)))
+
+
+
+// gsap.to(list,{
+//     x: "-50%",
+//     repeat: -1,
+//     duration: 5,
+//     ease: "linear"
+// })
+
