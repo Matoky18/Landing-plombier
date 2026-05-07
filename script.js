cd41fdde31d220d0f7e2f4eedfa8e78f72ca9ce6
Matoky18 <matokyranay@gmail.com>
2026-05-07 08:43:00 +0300

    active menu mobile

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-// const container = document.querySelector('.marquee');
-// const list = container.querySelector('ul');
-// const items = [...list.querySelectorAll('li')];
-// items.forEach(item=>list.append(item.cloneNode(true)))
+const hamburger = document.querySelector(".hamburger");
+const navMobile = document.querySelector(".nav-mobile");
+const xMark = document.querySelector(".x-mark")
+const li = document.querySelectorAll(".nav-mobile li")
 
 
 
-// gsap.to(list,{
-//     x: "-50%",
-//     repeat: -1,
-//     duration: 5,
-//     ease: "linear"
-// })
+hamburger.addEventListener("click",()=>{
+    console.log(navMobile,window.innerWidth)
+    toggleMenu()
 
+})
+
+function toggleMenu () {
+    if (window.innerWidth < 1024) {
+        navMobile.classList.toggle("active")    
+        document.body.style.overflow = "hidden"
+    }
+}
+
+function closeMenu () {
+    navMobile.classList.remove("active")
+    document.body.style.overflow = "auto"
+}
+
+xMark.addEventListener("click",closeMenu)
+
+li.forEach(li=> {
+    li.addEventListener("click",closeMenu)
+})
